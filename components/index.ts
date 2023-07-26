2edb2020fb5618b252a2bbcabe2c0b421aa286cf
Enauro-web <rortiz@enauro.com>
2023-07-26 08:58:11 +0200

    firstCommit

---
+++ components/index.ts
@@ -0,0 +1,22 @@
+import Hero from './Hero'
+import CustomButton from './CustomButton'
+import Navbar from './Navbar'
+import Footer from './Footer'
+import CustomFilter from './CustomFilter'
+import SearchBar from './SearchBar'
+import CarCard from './CarCard'
+import SearchManufacturer from './SearchManufacturer'
+import CardDetails from './CardDetails'
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    CustomFilter,
+    SearchBar,
+    CarCard,
+    SearchManufacturer,
+    CardDetails,
+    Footer
+    
+}
